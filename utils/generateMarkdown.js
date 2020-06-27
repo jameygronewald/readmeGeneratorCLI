8e3f40276ad636060d71236fee34e65e56757893
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-26 19:16:36 -0700

    filled in correct license info in switch case for all 4 licenses and confirmed apache and MIT are working

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -29,15 +29,29 @@ const generateMarkdown = data => {
       badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
       break;
     case 'Apache License 2.0':
-      license = 'Apache License';
+      license = '[Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0.txt)';
       badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
       break;
     case 'GNU GPLv3':
-      license = 'GNU';
+      license = '[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html)';
       badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
       break;
     case 'ISC License':
-      license = 'ISC';
+      license = `ISC License
+
+      Copyright (c) [${data.year}], [${data.name}]
+      
+      Permission to use, copy, modify, and/or distribute this software for any
+      purpose with or without fee is hereby granted, provided that the above
+      copyright notice and this permission notice appear in all copies.
+      
+      THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+      WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+      MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+      ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+      WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+      ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+      OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`;
       badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
       break;
   }
